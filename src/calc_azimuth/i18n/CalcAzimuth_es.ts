68ca82da7cc8915a31a8e593288c7a1599db2be4
Jose Manuel Mira Martínez <josema.mira@gmail.com>
2019-03-01 08:59:43 +0100

    Add files via upload

---
+++ src/calc_azimuth/i18n/CalcAzimuth_es.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>CalcAzimuthDialogBase</name>
+    <message>
+        <location filename="../calc_azimuth_dialog_base.ui" line="14"/>
+        <source>Calculate Azimuth</source>
+        <translation>Calcular rumbo/azimut</translation>
+    </message>
+    <message>
+        <location filename="../calc_azimuth_dialog_base.ui" line="107"/>
+        <source>Azimuth</source>
+        <translation>Rumbo</translation>
+    </message>
+    <message>
+        <location filename="../calc_azimuth_dialog_base.ui" line="81"/>
+        <source>Start</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <location filename="../calc_azimuth_dialog_base.ui" line="94"/>
+        <source>End</source>
+        <translation>Fin</translation>
+    </message>
+    <message>
+        <location filename="../calc_azimuth_dialog_base.ui" line="130"/>
+        <source>Distance</source>
+        <translation>Distancia</translation>
+    </message>
+</context>
+</TS>
---
+++ src/calc_azimuth/i18n/traduccion.pro
@@ -0,0 +1,4 @@
+FORMS = ../calc_azimuth_dialog_base.ui
+
+
+TRANSLATIONS = CalcAzimuth_es.ts
